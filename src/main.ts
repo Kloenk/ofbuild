ed21283a8bc58b6514883e6c276cba9499ad8501
Finn Behrens <kloenk@Finns-MBP.fritz.box>
2020-08-04 14:02:12 +0200

    add build flag

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,23 @@
 import * as core from '@actions/core'
-//const github = require('@actions/github');
 import * as github from '@actions/github'
 import * as Webhooks from '@octokit/webhooks'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    /*const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.info(github.event);
-
-    core.setOutput('time', new Date().toTimeString())*/
-
-    /*if (github.context.eventName === 'push') {
-      const pushPayload = github.context.payload; // as Webhooks.WebhookPayloadPush;
-      core.info(`The head commit is: ${JSON.stringify(pushPayload)}`);
-    } else {
-      core.info(`foobar: ${JSON.stringify(github.context.payload)}`);
-    };*/
     const packages = await parse_title();
     core.setOutput('package', packages.join(", "));
+
+    const build = core.getInput("build");
+    for (var target in packages) {
+      core.startGroup(`OfBuild: build ${target}`);
+      if (build) {
+        await exec.exec('nix-build', ['.', '-A', target]);
+      } else {
+	core.warning(`would run 'nix-build . -A ${target}'`);
+      }
+      core.endGroup();
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
@@ -34,11 +27,11 @@ async function parse_title(): Promise<Array<string>> {
     const context = github.context.payload;
     if (context.hasOwnProperty('pull_request')) {
       const title = String(context.pull_request!.title);
-      core.info(`title: ${context.pull_request!.title}`);
+      core.debug(`title: ${context.pull_request!.title}`);
 
       var split = title.split(":", 2);
       split = split[0].split(",");
-      core.info(`packages: ${split.join(" + ")}`);
+      core.debug(`packages: ${split.join(" + ")}`);
       return split;
     } else {
       //core.info(`object: ${JSON.stringify(github)}`);
